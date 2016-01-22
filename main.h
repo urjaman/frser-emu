ab3da45feca474d81d780088a020f3a4d089a921
Urja Rannikko <urjaman@gmail.com>
2016-01-23 00:45:58 +0200

    Use new libfrser and have less phony defines

--- main.h
+++ main.h
@@ -5,7 +5,4 @@
 #include <setjmp.h>
 #include "pgmspace-compat.h"
 
-#define RAMSTART 0
-#define RAMEND 0x2000
-
 #define _delay_us(x)
--- uart.h
+++ uart.h
@@ -5,12 +5,11 @@ uint8_t uart_recv(void);
 void uart_send(uint8_t val);
 
 
-#define BAUD 115200
-
 #define RECEIVE() uart_recv()
 #define SEND(n) uart_send(n)
-#define UART_BUFLEN 2048
-#define UARTTX_BUFLEN 0
+
+/* For the protocol window size */
+#define UART_BUFLEN 4096
 
 int server_init(uint16_t port);
 
