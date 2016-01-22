70abb36321e3a0f950392871e7a57ebd8b0975a3
Urja Rannikko <urjaman@gmail.com>
2016-01-22 23:14:18 +0200

    use newer libfrser and its pgmspace compat header

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
-#include <avr/pgmspace.h>
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
 #include <inttypes.h>
 #include <setjmp.h>
+#include "pgmspace-compat.h"
 
 #define RAMSTART 0
 #define RAMEND 0x2000
